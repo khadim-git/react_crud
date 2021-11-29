00290df124ba8bc723854ecb2e1782bd17146b7f
khadim-git <59868394+khadim-git@users.noreply.github.com>
2021-11-29 16:24:11 +0500

    Update CreateFrom.js

--- src/components/Books/CreateBook/CreateFrom.js
+++ src/components/Books/CreateBook/CreateFrom.js
@@ -1,7 +1,7 @@
 import { React, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import  Config from '../../Config/Config'
+import  Config from '../../Config'
 
 const  CreatBook = () =>{
   const navigate = useNavigate();
