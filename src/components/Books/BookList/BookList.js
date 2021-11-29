03982f84a51796d84dbae1399dabcbcd6be265ee
khadim-git <59868394+khadim-git@users.noreply.github.com>
2021-11-29 16:23:27 +0500

    Update BookList.js

--- src/components/Books/BookList/BookList.js
+++ src/components/Books/BookList/BookList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import { useEffect , useState } from 'react';
 import { Link } from 'react-router-dom'
-import  Config from '../../Config/Config'
+import  Config from '../../Config'
 
 const  BookList = () =>{
   const [data,setData] = useState([])
